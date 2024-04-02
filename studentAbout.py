38865d98bb7949808bee102af0b79e0d56c1cd9a
Istem Karakas <istem.karakas@gmail.com>
2024-04-02 09:24:14 +0000

    Add student about class

---
+++ studentAbout.py
@@ -0,0 +1,9 @@
+class StudentAbout:
+
+    def __init__(self, name, surname, lesson, point_one, point_two, statu) -> None:
+        self.name = name
+        self.surname = surname
+        self.lesson = lesson
+        self.point_one = point_one
+        self.point_two = point_two
+        self.statu = statu
