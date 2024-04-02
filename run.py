2b985bfcfb27cad16ca65b74c02df22e82a30344
Istem Karakas <istem.karakas@gmail.com>
2024-04-02 09:27:20 +0000

    Add basic user parameter function

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import csv
+from studentAbout import StudentAbout
+
+def user_parameter():
+    print("Welcome Automation")
+    student_name = input("Enter Student Name: ")
+    student_surname = input("Enter Student Surname: ")
+    
+    print("Select a lesson : ")
+    student_lesson = [
+        "English",
+        "Science",
+        "Math",
+        "History",
+    ]
+    for i, lesson_name in enumerate(student_lesson):
+        print(f" {i + 1}.{lesson_name}")
+
