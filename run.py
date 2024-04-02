407196cf288c11a7a6e76f289ea8b49c112bdd8a
Istem Karakas <istem.karakas@gmail.com>
2024-04-02 09:30:24 +0000

    Add questions for user exam scores

--- run.py
+++ run.py
@@ -16,3 +16,10 @@ def user_parameter():
     for i, lesson_name in enumerate(student_lesson):
         print(f" {i + 1}.{lesson_name}")
 
+    value_range = f"[1 - {len(student_lesson)}]"
+    selected_index = int(input(f"Enter a lesson number {value_range}: ")) - 1
+    
+    student_point_one = int(input("Enter Quiz One Point: "))
+    student_point_two = int(input("Enter Quiz Two Point : "))
+
+    if((student_point_one+student_point_two)/2>40):
