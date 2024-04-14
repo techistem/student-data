b5f10864861d12db1495d2002f98bf4e30be5dec
Istem Karakas <istem.karakas@gmail.com>
2024-04-14 04:06:47 +0000

    PEP8 Python Linter control structures and codes have been edited

--- run.py
+++ run.py
@@ -11,10 +11,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('student-data')
 
+
 class StudentData():
     """A class to represent student data."""
 
-    def __init__(self, name, surname, lesson, score_one, score_two, status) -> None:
+    def __init__(
+         self, name, surname, lesson, score_one, score_two, status) -> None:
         self.name = name
         self.surname = surname
         self.lesson = lesson
@@ -22,11 +24,15 @@ class StudentData():
         self.score_two = score_two
         self.status = status
 
-def user_parameter():
-    """Collects user input for student information and returns a StudentData object.
 
-    This function prompts the user to enter the student's name, surname, lesson, exam scores,
-    and calculates the student's status based on their average score."""
+def user_parameter():
+    """
+    Collects user input for student information and returns a StudentData
+    object.
+    This function prompts the user to enter the student's name, surname,
+    lesson, exam scores, and calculates the student's status based on their
+    average score.
+    """
 
     print("Welcome Automation")
     student_name = None
@@ -36,15 +42,19 @@ def user_parameter():
         if student_name.isalpha() and len(student_name) > 1:
             break
         else:
-            print(f"{student_name} is invalid. Please use only letters and must be longer than 1 character.")
+            print(
+                f"{student_name} is invalid. Please use only
+                letters and must be longer than 1 character.")
 
     while True:
         student_surname = input("Enter Student Surname: ")
         if student_surname.isalpha() and len(student_surname) > 1:
             break
         else:
-            print(f"{student_surname} is invalid. Please use only letters and must be longer than 1 character..")
-    
+            print(
+                f"{student_surname} is invalid. Please use only letters and
+                must be longer than 1 character..")
+
     print("Select a lesson : ")
     student_lesson = [
         "English",
@@ -60,10 +70,11 @@ def user_parameter():
         user_input = input(f"Enter a lesson number {value_range}:")
         if user_input.isdigit():
             selected_index = int(user_input) - 1
-            if 0 <= selected_index<len(student_lesson):
-                break
+        if 0 <= selected_index < len(student_lesson):
+            break
             else:
-                print("Invalid lesson number.Please enter a valid lesson number.")
+                print(
+                  "Invalid lesson number.Please enter a valid lesson number.")
         else:
             print("Invalid input. Please enter a valid lesson number.")
 
@@ -73,7 +84,8 @@ def user_parameter():
             student_exam_score_one = int(student_exam_score_one)
             break
         else:
-            print(f"{student_exam_score_one} is invalid. Please use only numbers.")
+            print(
+             f"{student_exam_score_one} is invalid. Please use only numbers.")
 
     while True:
         student_exam_score_two = input("Enter Exam Score Two:")
@@ -81,23 +93,32 @@ def user_parameter():
             student_exam_score_two = int(student_exam_score_two)
             break
         else:
-            print(f"{student_exam_score_two} is invalid. Please use only numbers.")
+            print(
+             f"{student_exam_score_two} is invalid. Please use only numbers.")
 
-    if(student_exam_score_one +student_exam_score_two) / 2 > 40:
+    if (student_exam_score_one + student_exam_score_two) / 2 > 40:
         student_status = "Passed"
     else:
-        student_status = "Fail"    
+        student_status = "Fail"
 
     if selected_index in range(len(student_lesson)):
         selected_lesson = student_lesson[selected_index]
-        return StudentData(name=student_name, surname=student_surname, lesson=selected_lesson, score_one=student_exam_score_one, score_two=student_exam_score_two, status=student_status)
+        return StudentData(
+            name=student_name, surname=student_surname, lesson=selected_lesson,
+            score_one=student_exam_score_one, score_two=student_exam_score_two,
+            status=student_status)
     else:
         print("Invalid lesson. Please try again!")
+
 
 def save_to_sheet(studentdata):
     """Saves student data to a Google Sheets worksheet."""
     worksheet = SHEET.worksheet("studentData")
-    worksheet.append_row([studentdata.name, studentdata.surname, studentdata.lesson, studentdata.score_one, studentdata.score_two, studentdata.status])
+    worksheet.append_row([
+                          studentdata.name, studentdata.surname,
+                          studentdata.lesson, studentdata.score_one,
+                          studentdata.score_two,
+                          studentdata.status])
     print("New student data added successfully!")
 
 
@@ -115,8 +136,12 @@ def student_data_gspread_list():
         print("Student Lesson Status:", row[5])
         print()
 
+
 def main():
-    """This function provides a menu-driven interface for interacting with student data."""
+    """
+    This function provides a menu-driven interface for interacting with
+    student data.
+    """
     while True:
         print("What do you want to do?")
         print("1. Add a student data")
@@ -136,6 +161,7 @@ def main():
             break
         else:
             print("Invalid choice. Please enter 1, 2, or 3.")
+
 
 if __name__ == "__main__":
     main()
